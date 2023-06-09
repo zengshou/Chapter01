27c165478e6907b3462eb1f6f3ac0604c4a74a1f
zengshou <3090368266@qq.com>
2023-06-09 20:25:38 +0800

    2

--- src/main/java/cn/lzzy/ChapterApplication.java
+++ src/main/java/cn/lzzy/ChapterApplication.java
@@ -4,5 +4,6 @@ public class ChapterApplication {
     public static void main(String[] args) {
         System.out.println("大家，下午好！");
         System.out.println("hao");
+        System.out.println("我是hallo word");
     }
 }
