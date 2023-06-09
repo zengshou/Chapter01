27113b34ac06ea80e3336f2229e3fcf67d4d085e
zengshou <3090368266@qq.com>
2023-06-09 16:46:42 +0800

    1

---
+++ src/main/java/cn/lzzy/ChapterApplication.java
@@ -0,0 +1,8 @@
+package cn.lzzy;
+
+public class ChapterApplication {
+    public static void main(String[] args) {
+        System.out.println("大家，下午好！");
+        System.out.println("hao");
+    }
+}
